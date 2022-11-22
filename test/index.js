ddda1f6bdc6daf591693fee6d2965ff03dee3121
Titus Wormer <tituswormer@gmail.com>
2022-11-22 14:00:51 +0100

    Use Node test runner

--- test/index.js
+++ test/index.js
@@ -1,8 +1,9 @@
-import {promises as fs} from 'node:fs'
-import test from 'tape'
+import assert from 'node:assert/strict'
+import fs from 'node:fs/promises'
+import test from 'node:test'
 import {createGfmFixtures} from '../index.js'
 
-test('create-gfm-fixtures', async (t) => {
+test('create-gfm-fixtures', async () => {
   const fixtures = new URL('fixtures/', import.meta.url)
   await fs.mkdir(fixtures, {recursive: true})
 
@@ -15,12 +16,12 @@ test('create-gfm-fixtures', async (t) => {
 
   await createGfmFixtures(fixtures)
 
-  t.equal(
+  assert.equal(
     String(await fs.readFile(new URL('example.html', fixtures))),
     '<p>a\nb</p>\n',
     'should crawl html (file)'
   )
-  t.equal(
+  assert.equal(
     String(await fs.readFile(new URL('example.comment.html', fixtures))),
     '<p>a<br>\nb</p>\n',
     'should crawl html (comment)'
@@ -33,7 +34,7 @@ test('create-gfm-fixtures', async (t) => {
 
   const statsAfter = await fs.stat(new URL('example.html', fixtures))
 
-  t.equal(
+  assert.equal(
     statsBefore.mtimeMs,
     statsAfter.mtimeMs,
     'should not modify existing files'
@@ -54,7 +55,7 @@ test('create-gfm-fixtures', async (t) => {
 
   await createGfmFixtures(fixtures)
 
-  t.equal(
+  assert.equal(
     String(await fs.readFile(new URL('example.comment.html', fixtures))),
     '<p>a<br>\nb</p>\n',
     'should crawl w/ just comments'
@@ -68,7 +69,7 @@ test('create-gfm-fixtures', async (t) => {
 
   await createGfmFixtures(fixtures, {controlPictures: true})
 
-  t.equal(
+  assert.equal(
     String(await fs.readFile(new URL('control.html', fixtures))),
     '<p>a<br>\nb</p>\n',
     'should crawl w/ `controlPictures`'
@@ -100,66 +101,64 @@ test('create-gfm-fixtures', async (t) => {
 
   await createGfmFixtures(fixtures)
 
-  t.equal(
+  assert.equal(
     String(await fs.readFile(new URL('dir.html', fixtures))),
     '<p>a</p>\n',
     'should clean `dir` attributes'
   )
 
-  t.equal(
+  assert.equal(
     String(await fs.readFile(new URL('mention.file.html', fixtures))),
     '<p>@wooorm</p>\n',
     'should clean mentions (1, not supported in files)'
   )
-  t.equal(
+  assert.equal(
     String(await fs.readFile(new URL('mention.comment.html', fixtures))),
     '<p><a href="https://github.com/wooorm">@wooorm</a></p>\n',
     'should clean mentions (2, supported in files)'
   )
 
-  t.equal(
+  assert.equal(
     String(await fs.readFile(new URL('heading-anchor.html', fixtures))),
     '<h1>hi</h1>\n',
     'should clean heading (link-to-self and anchor icon)'
   )
 
-  t.equal(
+  assert.equal(
     String(await fs.readFile(new URL('link.html', fixtures))),
     '<p><a href="https://bravo.com">alpha</a>, <a href="/delta">charlie</a>, <a href="./foxtrot">echo</a>, <a href="?hotel">golf</a>, <a href="#juliett">india</a>.</p>\n',
     'should clean links'
   )
 
-  t.equal(
+  assert.equal(
     String(await fs.readFile(new URL('image.html', fixtures))),
     '<p><img src="https://bravo.com" alt="alpha">, <img src="/delta" alt="charlie">, <img src="./foxtrot" alt="echo">, <img src="" alt="golf">, <img src="juliett?kilo" alt="india">, <img src="" alt="lima">, <img src="papa#qu%C3%A9bec" alt="oscar">, <a href="uniform"><img src="tango" alt="sierra"></a>.</p>\n',
     'should clean images'
   )
 
-  t.equal(
+  assert.equal(
     String(await fs.readFile(new URL('emoji.html', fixtures))),
     '<p>✅</p>\n',
     'should clean emoji'
   )
 
-  t.equal(
+  assert.equal(
     String(await fs.readFile(new URL('footnote.html', fixtures))),
     '<p><sup><a href="#user-content-fn-a" id="user-content-fnref-a" data-footnote-ref="" aria-describedby="footnote-label">1</a></sup></p>\n<section data-footnotes="" class="footnotes"><h2 id="footnote-label" class="sr-only">Footnotes</h2>\n<ol>\n<li id="user-content-fn-a">\n<p>b <a href="#user-content-fnref-a" data-footnote-backref="" aria-label="Back to content" class="data-footnote-backref">↩</a></p>\n</li>\n</ol>\n</section>\n',
     'should clean footnotes'
   )
 
-  t.equal(
+  assert.equal(
     String(await fs.readFile(new URL('tasklist.html', fixtures))),
     '<ul>\n<li><input type="checkbox" disabled checked> a</li>\n</ul>\n',
     'should clean tasklists'
   )
 
-  t.equal(
+  assert.equal(
     String(await fs.readFile(new URL('frontmatter.html', fixtures))),
     '<h1>c</h1>\n',
     'should clean frontmatter'
   )
 
   await fs.rmdir(fixtures, {recursive: true})
-
-  t.end()
 })
